95744f8cfdf1ee19ae81523515c9ee94ba7439ae
Baptiste Parsy <baptiste.parsy@epita.fr>
2020-05-19 09:02:49 +0200

    Add debug opengl msg callback

--- src/engine/WinHandler.cc
+++ src/engine/WinHandler.cc
@@ -7,6 +7,18 @@
 #include <GL/glew.h>
 #include "WinHandler.hh"
 
+[[maybe_unused]] static void GLAPIENTRY
+MessageCallback([[maybe_unused]]GLenum source,
+                GLenum type,
+                [[maybe_unused]]GLuint id,
+                GLenum severity,
+                [[maybe_unused]]GLsizei length,
+                const GLchar* message,
+                [[maybe_unused]]const void* userParam) {
+    fprintf(stderr, "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n",
+            (type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : ""), type, severity, message);
+}
+
 WinHandler::WinHandler(int width, int height) : width_(width), height_(height) {
     if (!glfwInit())
         throw std::runtime_error("Could not initialize GLFW");
@@ -23,6 +35,11 @@ WinHandler::WinHandler(int width, int height) : width_(width), height_(height) {
     std::cout << "Using OpenGL version: " << glGetString(GL_VERSION) << std::endl;
     // Baptiste's machine has "4.6.0 NVIDIA 440.82"
     // Salome's machine has "?"
+    
+#ifdef DEBUG_OPENGL
+    glEnable(GL_DEBUG_OUTPUT);
+    glDebugMessageCallback(MessageCallback, 0);
+#endif
 }
 
 WinHandler::~WinHandler() {
