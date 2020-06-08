0a41472dc6a867f5fdb1ce05b12faed6789870d8
Baptiste Parsy <baptiste.parsy@epita.fr>
2020-06-08 11:14:02 +0200

    VertexBuffer accept both arrays and vectors

--- src/engine/buffers/VertexBuffer.hh
+++ src/engine/buffers/VertexBuffer.hh
@@ -17,23 +17,39 @@ public:
     using vec = std::vector<ptr>;
 public:
     VertexBuffer() : BaseBuffer() {}
+
     VertexBuffer(VertexBuffer&&) = default;
     virtual ~VertexBuffer();
     void bind() const override;
     void unbind() const override;
 
+    template<typename T>
+    static VertexBuffer::ptr create(T* data, std::size_t size);
+
     template<typename T, std::size_t D>
     static ptr create(std::array<T, D> const& data);
+
+    template<typename T>
+    static ptr create(std::vector<T> const& data);
 };
 
-template<typename T, std::size_t D>
-VertexBuffer::ptr VertexBuffer::create(const std::array<T, D>& data) {
+template<typename T>
+VertexBuffer::ptr VertexBuffer::create(T* data, std::size_t size) {
     auto vb = std::make_unique<VertexBuffer>();
     glGenBuffers(1, &vb->id_);
     vb->bind();
-    glBufferData(GL_ARRAY_BUFFER, D * sizeof(T), data.data(), GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, size * sizeof(T), data, GL_STATIC_DRAW);
     return vb;
 }
 
+template<typename T, std::size_t D>
+VertexBuffer::ptr VertexBuffer::create(const std::array<T, D>& data) {
+    return create(data.data(), D);
+}
+
+template<typename T>
+VertexBuffer::ptr VertexBuffer::create(const std::vector<T>& data) {
+    return create(data.data(), data.size());
+}
 
 #endif //FIRE_VERTEXBUFFER_HH
