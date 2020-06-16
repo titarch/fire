023427949a92756f13b57bc5485577beac5cbabf
Spinoza <salome.desauty@epita.fr>
2020-06-16 16:49:18 +0200

    [FIX] include paths

--- src/engine/textures/texture.cc
+++ src/engine/textures/texture.cc
@@ -1,6 +1,6 @@
 #define STB_IMAGE_IMPLEMENTATION
-#include "utils/stb_image.h"
-#include "buffers/VertexBuffer.hh"
+#include "../../utils/stb_image.h"
+#include "../buffers/VertexBuffer.hh"
 #include "texture.hh"
 
 
@@ -8,8 +8,8 @@ Texture::Texture(std::string file_path)
 : file_path(file_path), width(0), height(0), local_buffer(nullptr), bytes_per_pixels(0), render_id(0)
 {
     // open file , get width & height
-    //stbi_set_flip_vertically_on_load(1);
-    //local_buffer = stbi_load(file_path.c_str(), &width, &height, &bytes_per_pixels, 4);
+    stbi_set_flip_vertically_on_load(1);
+    local_buffer = stbi_load(file_path.c_str(), &width, &height, &bytes_per_pixels, 4);
     
     glGenTextures(1, &render_id);
     glBindTexture(GL_TEXTURE_2D, render_id);
