4fd58114795088fc2e7fecca2cd9ef4d3026ef9f
Baptiste Parsy <baptiste.parsy@epita.fr>
2020-06-17 11:52:45 +0200

    Add engine/particles and created Particle class

---
+++ src/engine/particles/Particle.cc
@@ -0,0 +1,5 @@
+//
+// Created by bparsy on 6/17/20.
+//
+
+#include "Particle.hh"
---
+++ src/engine/particles/Particle.hh
@@ -0,0 +1,14 @@
+//
+// Created by bparsy on 6/17/20.
+//
+
+#ifndef FIRE_PARTICLE_HH
+#define FIRE_PARTICLE_HH
+
+
+class Particle {
+
+};
+
+
+#endif //FIRE_PARTICLE_HH
