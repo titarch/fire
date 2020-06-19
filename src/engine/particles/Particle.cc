fe346465906525a56c18a64d769bff9a05189cb4
Baptiste Parsy <baptiste.parsy@epita.fr>
2020-06-19 17:33:09 +0200

    Fix particle scaling error

--- src/engine/particles/Particle.cc
+++ src/engine/particles/Particle.cc
@@ -39,18 +39,18 @@ void Particle::init_program() {
 
 void Particle::update_physics(float dt) {
     if (energy_ <= 0.f) return;
-    if (direction_change_ <= 0)
-    {
+    if (direction_change_ <= 0) {
         float random_x = (float) ((rand() % 100) - 50) / 200.0f;
         float random_y = (float) ((rand() % 100) - 50) / 200.0f;
         float random_z = (float) ((rand() % 100) - 50) / 200.0f;
-        direction_change_ = 20 * (0.5f + ((float)rand() / (float)RAND_MAX) / 2.f);
+        direction_change_ = 20 * (0.5f + ((float) rand() / (float) RAND_MAX) / 2.f);
         rand_vel = {random_x, random_y, random_z};
         if (energy_ - direction_change_ <= 0)
             vel_ = {0, 0.5, 0};
         rand_vel = rand_vel + vel_;
         size_ /= 2;
-        program_->set_uniform<GL_FLOAT_VEC4>("u_scale", size_, size_, size_, size_);
+        program_->use();
+        program_->set_uniform<GL_FLOAT_VEC4>("u_scale", size_, size_, size_, 1.f);
     }
     pos_ += rand_vel * dt;
     alpha_ -= dt * 0.5;
@@ -65,8 +65,8 @@ void Particle::respawn(Vec const& position) {
     float rColor = 0.1f + ((float) (rand() % 100) / 500.0f);
     pos_ = {position[0] + random_x, position[1], position[2] + random_z};
     color_ = {rColor, rColor / 3.f, 0};
-    energy_ = 60.0f * (0.5f + ((float)rand() / (float)RAND_MAX) / 2.f);
-    direction_change_ = 30 * (0.5f + ((float)rand() / (float)RAND_MAX) / 2.f);
+    energy_ = 60.0f * (0.5f + ((float) rand() / (float) RAND_MAX) / 2.f);
+    direction_change_ = 30 * (0.5f + ((float) rand() / (float) RAND_MAX) / 2.f);
     vel_ = {0, 0.5, 0};
     vel_ -= pos_;
     alpha_ = 1.f;
